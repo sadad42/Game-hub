4960f3c5e3de31458e8ae20edf8bde709a1dcd2c
ajabdi <libaaxasad631@gmail.com>
2023-10-31 09:09:14 +0000

    amending files

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,4 @@
 import { Card, CardBody, Heading, HStack, Image, Text } from '@chakra-ui/react'
-import React from 'react'
 import { Game } from '../hooks/useGame'
 import getCroppedImageUrl from '../services/image-url'
 import CriticScore from '../components/criticScore'
