c73106ffc3e864ba2eb8676cefb1ff1798a6a2f5
ajabdi <libaaxasad631@gmail.com>
2023-10-31 15:16:50 +0000

    refining the gamehub

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "c7b18323a47d40c394ea5b019646b1f5",
+    key: "b8c9379b7c21463494915f8334c4f7d2",
   },
 });
